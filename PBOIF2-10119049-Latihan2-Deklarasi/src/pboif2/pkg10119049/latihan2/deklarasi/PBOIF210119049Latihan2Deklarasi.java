fde145adefcfca4aad6461886d3b565f62a86dac
Nur Sasongko <32429721+ssasongko@users.noreply.github.com>
2020-10-15 21:16:54 +0700

    Add files via upload

---
+++ PBOIF2-10119049-Latihan2-Deklarasi/src/pboif2/pkg10119049/latihan2/deklarasi/PBOIF210119049Latihan2Deklarasi.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119049.latihan2.deklarasi;
+
+/**
+ *
+ * @author 
+ * Nama  : Nur Sasongko
+ * Kelas : PBOIF2
+ * NIM   : 10119049
+ * Deskripsi Program : Program ini berisi Deklarasi dan Pengaksesan Variabel
+ */
+public class PBOIF210119049Latihan2Deklarasi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int nilaiInt;
+        final double PHI = 3.14;
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        // Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("Isi variabel karakter = " + nilaiKarakter);
+    }
+    
+}
